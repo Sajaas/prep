e6e2be08d836dc0f67eac4314313f3dfa7e2a45a
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-02 15:56:12 +0200

    added .each, removed my_group = new.Array

--- week_2/my_group.rb
+++ week_2/my_group.rb
@@ -1,7 +1,9 @@
-my_group = new.Array
-
-person_1 = {name: "Ali", gender: "Male", age: 34}
-person_2 = {name: "Alex", gender: "Non-binary", age: 22}
-person_3 = {name: "Abena", gender: "Female", age: 56}
+person_1 = {name: "Ali", gender: "male", age: 34}
+person_2 = {name: "Alex", gender: "non-binary", age: 22}
+person_3 = {name: "Abena", gender: "female", age: 56}
 
 my_group = [person_1, person_2, person_3]
+
+my_group.each do |person|
+  puts "#{person[:name]} is a #{person[:age]} years old #{person[:gender]}"
+end
