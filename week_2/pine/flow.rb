e9a005c13be3b9e95125a3d9d678cb77a8089755
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-06 08:04:44 +0200

    Updated the song code

--- week_2/pine/flow.rb
+++ week_2/pine/flow.rb
@@ -1,30 +1,31 @@
-#"99 bottles of beer on the wall..."
+# "99 bottles of beer on the wall..."
 
 bottles_on_wall = 99
 bottles_on_wall_after = 98
 
-while bottles_on_wall > 0
+while bottles_on_wall >= 0
+  if bottles_on_wall > 1
   puts "#{bottles_on_wall} bottles of beer on the wall, #{bottles_on_wall} bottles of beer.
 Take one down and pass it around, #{bottles_on_wall_after} bottles of beer on the wall."
   puts ' '
-  bottles_on_wall -= 1
-  bottles_on_wall_after -= 1
+end
   if bottles_on_wall == 1
     puts '1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.'
     puts ' '
-    bottles_on_wall -= 1
-    bottles_on_wall_after -= 1
-  end
 end
-
-if bottles_on_wall == 0
-    puts 'No more bottles of beer on the wall, no more bottles of beer.
+  if bottles_on_wall == 0
+      puts 'No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.'
-puts ' '
+  puts ' '
+  end
+  bottles_on_wall -= 1
+  bottles_on_wall_after -= 1
 end
 
-#Deaf Grandma program
+
+
+# Deaf Grandma program
 puts 'Talk to your deaf Grandma'
 response = ''
 while response != 'BYE'
