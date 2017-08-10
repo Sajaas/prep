9c041e2fdb9d3a3b8d5e96009f3a43b2a30b98eb
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-10 11:13:42 +0200

    Added testing for sad_path, but still working on method for sad_path

--- week_3/fizz_buzz/spec/fizz_buzz_spec.rb
+++ week_3/fizz_buzz/spec/fizz_buzz_spec.rb
@@ -13,4 +13,13 @@ describe 'fizz_buzz' do
   it "returns 'fizz buzz' if number is divisible by 15" do
     expect(fizz_buzz(15)).to eq 'fizz buzz'
   end
+  it "returns 'this is a string' if user enters a string" do
+    expect(fizz_buzz(String)).to eq 'this is a string'
+  end
+  it "returns 'this is a float' if user enters a float" do
+    expect(fizz_buzz(Float)).to eq 'this is a float'
+  end
+  it "returns 'this is a negative number' if user enters a negative number" do
+    expect(fizz_buzz("negative number")).to eq 'this is a negative number'
+  end
 end
