420d036ac49707731d42ba2034356ffc58b866d5
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-10 10:47:41 +0200

    First try to add sad path

--- week_3/fizz_buzz/lib/fizz_buzz.rb
+++ week_3/fizz_buzz/lib/fizz_buzz.rb
@@ -9,3 +9,16 @@ def fizz_buzz(number)
     number
   end
 end
+# how do I add a sad_path?
+
+# sad_path if user inserts string, number < 0, or float instead of integer
+def sad_path(number)
+  case number
+  when String
+    'This is a string, not an integer'
+  when number < 0
+    'This is a negative number'
+  when Float
+    'This number has a fractional value, it is not an integer'
+  end
+end
